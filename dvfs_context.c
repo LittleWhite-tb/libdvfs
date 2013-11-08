f8388eda44870310b3d925792f8734b150cb826e
Benoit Pradelle <benoit.pradelle@uvsq.fr>
2013-11-08 15:10:18 +0100

    small fix

--- dvfs_context.c
+++ dvfs_context.c
@@ -243,15 +243,9 @@ static void get_related_cores(unsigned int id, unsigned int **cores, unsigned in
       unsigned int nval;
       char sep;
 
-      fscanf(fd, "%c", &sep);
+      int fret = fscanf(fd, "%c", &sep);
       
-      // Try to treat the first file format (0 1 2 ... N)
-      if ( sep == '\n' ) // File finished, we guess, we have count everything
-      {
-         (*nb_cores)++; // Last core to count
-         break; // Leave
-      }
-      if (sep == ' ' || sep == ',') {
+      if (fret == EOF || fret == 0 || sep == ' ' || sep == ',' || sep == '\n') {
          (*nb_cores)++;
          continue;
       }
